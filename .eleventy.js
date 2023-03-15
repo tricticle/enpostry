9859e6a0a3054a6c124535e10c9a9c7c07817560
tricticle <95101379+tricticle@users.noreply.github.com>
2023-03-16 02:16:54 +0530

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/assets/');
     eleventyConfig.addPassthroughCopy('./src/blogs/');
     eleventyConfig.addPassthroughCopy('./src/tricticle/');
+eleventyConfig.addPassthroughCopy('./src/Ads.txt/');
     return {
         dir:{
             input: "src",
