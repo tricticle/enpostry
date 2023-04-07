6da0313add889f8f7990cb39891ee392af9a6f71
tricticle <tricticle@gmail.com>
2023-04-07 14:18:55 +0530

    adding pwa

--- .eleventy.js
+++ .eleventy.js
@@ -7,6 +7,8 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/tricticle/');
     eleventyConfig.addPassthroughCopy('./src/ads.txt');
     eleventyConfig.addPassthroughCopy('./src/Product\ Sans\ Bold.ttf');
+    eleventyConfig.addPassthroughCopy('./src/manifest.json');
+    eleventyConfig.addPassthroughCopy('./src/service-worker.js');
 
 
     return {
